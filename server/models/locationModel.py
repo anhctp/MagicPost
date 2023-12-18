725d93bd12de626ca3d9b5805ef63478c156dd15
ctpanh <ctpanh03@gmail.com>
2023-12-18 22:09:29 +0700

    fix: query join get information of location

--- server/models/locationModel.py
+++ server/models/locationModel.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint
 
 class LocationModel(database.Base):
     __tablename__ = "locations"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, unique=True)
     ward_id = Column(Integer, ForeignKey("wards.id"))
     address = Column(String(100), nullable=True)
     __table_args__ = (UniqueConstraint('ward_id', 'address', name='_ward_address_uc'),)
--- server/routes/locationRoute.py
+++ server/routes/locationRoute.py
@@ -11,12 +11,17 @@ router = APIRouter(
 )
 
 @router.get("/")
-def get_all_location(db: Session = Depends(getDatabase)):
-    return LocationController.getAllLocation(db=db)
+def get_all_location(page: int = 1, size: int = 10, db: Session = Depends(getDatabase)):
+    return LocationController.getAllLocation(page=page, size=size, db=db)
 
 @router.get("/data")
 async def set_data(db: Session = Depends(getDatabase)):
     return await LocationController.set_data(db=db)
+
+@router.get("/{location_id}")
+def get_location_by_id(location_id: int, db: Session = Depends(getDatabase)):
+    return LocationController.getLocationById(location_id=location_id, db=db)
+
 
 @router.post("/{ward_id}")
 def create_location(ward_id: int, location: CreateLocation, db: Session = Depends(getDatabase)):
