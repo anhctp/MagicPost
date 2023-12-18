3b08d4250fff7212da481a4755bce8d7f2a0566f
ctpanh <ctpanh03@gmail.com>
2023-12-18 22:09:29 +0700

    fix: query join get information of location

--- server/controllers/locationController.py
+++ server/controllers/locationController.py
@@ -1,5 +1,5 @@
-from fastapi import Depends, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session, aliased
 from database import getDatabase
 from models.locationModel import LocationModel
 from models.divisionModel import DivisionModel
@@ -22,8 +22,91 @@ async def get_data_from_api():
             raise HTTPException(status_code=500, detail=f"Network error: {str(error)}")
         
 class LocationController:
-    def getAllLocation(db: Session = Depends(getDatabase)):
-        return db.query(LocationModel).all()
+    def getAllLocation(page: int = 1, size: int = 10, db: Session = Depends(getDatabase)):
+        skip = (page - 1) * size
+        division_alias = aliased(DivisionModel, name="division")
+        district_alias = aliased(DistrictModel, name="district")
+        ward_alias = aliased(WardModel, name="ward")
+        locations = (
+            db.query(
+                LocationModel.id,
+                LocationModel.address,
+                ward_alias.name.label('ward_name'),
+                ward_alias.id.label('ward_id'),
+                district_alias.name.label('district_name'),
+                district_alias.id.label('district_id'),
+                division_alias.id.label('division_id'),
+                division_alias.name.label('division_name')
+            )
+            .join(ward_alias, LocationModel.ward_id == ward_alias.id)
+            .join(district_alias, ward_alias.district_id == district_alias.id)
+            .join(division_alias, district_alias.division_id == division_alias.id)
+            .offset(skip)
+            .limit(size)
+            .all())
+        result = []
+        for item in locations:
+            location = {
+            "id": item.id,
+            "address": item.address,
+            "ward": {
+                "id": item.ward_id,
+                "name": item.ward_name
+                },
+            "district": {
+                "id": item.district_id,
+                "name": item.district_name
+                },
+            "division": {
+                "id": item.division_id,
+                "name": item.division_name
+                }
+        }
+            result.append(location)
+        return result
+    def getLocationById(location_id: int, db: Session = Depends(getDatabase)):
+        division_alias = aliased(DivisionModel, name="division")
+        district_alias = aliased(DistrictModel, name="district")
+        ward_alias = aliased(WardModel, name="ward")
+
+        location_info = (
+            db.query(
+                LocationModel.id,
+                LocationModel.address,
+                ward_alias.name.label('ward_name'),
+                ward_alias.id.label('ward_id'),
+                district_alias.name.label('district_name'),
+                district_alias.id.label('district_id'),
+                division_alias.id.label('division_id'),
+                division_alias.name.label('division_name')
+            )
+            .join(ward_alias, LocationModel.ward_id == ward_alias.id)
+            .join(district_alias, ward_alias.district_id == district_alias.id)
+            .join(division_alias, district_alias.division_id == division_alias.id)
+            .filter(LocationModel.id == location_id)
+            .first()
+        )
+        if not location_info:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials"
+            )
+        result =  {
+            "id": location_info.id,
+            "address": location_info.address,
+            "ward": {
+                "id": location_info.ward_id,
+                "name": location_info.ward_name
+                },
+            "district": {
+                "id": location_info.district_id,
+                "name": location_info.district_name
+                },
+            "division": {
+                "id": location_info.division_id,
+                "name": location_info.division_name
+                }
+        }
+        return result
     
     def createLocation(ward_id: int, location: CreateLocation, db: Session = Depends(getDatabase)):
         db_location = LocationModel(
@@ -37,37 +120,43 @@ class LocationController:
     
             
     async def set_data(db: Session = Depends(getDatabase)):
-        data = await get_data_from_api()
-        for division in data:
-            db_division = DivisionModel(
-                name=division["name"],
-                type=division["division_type"]
-            )
-            db.add(db_division)
-            db.commit()
-            db.refresh(db_division)
-            for district in division["districts"]:
-                db_district = DistrictModel(
-                    division_id=db_division.id,
-                    name=district["name"],
-                    type=district["division_type"]
-                )
-                db.add(db_district)
-                db.commit()
-                db.refresh(db_district)
-                for ward in district["wards"]:
-                    db_ward = WardModel(
-                        district_id=db_district.id,
-                        name=ward["name"],
-                        type=ward["division_type"]
+        try:
+            data = await get_data_from_api()
+            with db.begin():
+                for division in data:
+                    db_division = DivisionModel(
+                        name=division["name"],
+                        type=division["division_type"]
                     )
-                    db.add(db_ward)
-                    db.commit()
-                    db.refresh(db_ward)
+                    db.add(db_division)
+                    db.flush()
+                    for district in division["districts"]:
+                        db_district = DistrictModel(
+                            division_id=db_division.id,
+                            name=district["name"],
+                            type=district["division_type"]
+                        )
+                        db.add(db_district)
+                        db.flush()
+                        for ward in district["wards"]:
+                            db_ward = WardModel(
+                                district_id=db_district.id,
+                                name=ward["name"],
+                                type=ward["division_type"]
+                            )
+                            db.add(db_ward)
+                            db.flush()
+                            print("district: ")
+                            print(db_district.division_id)
+                            print(db_district.name)
+                            print("ward: ")
+                            print(db_ward.id)
+                            print(db_ward.name)
+                            db_location = LocationModel(
+                                ward_id=db_ward.id,
+                            )
+                            db.add(db_location)
+                            db.flush()
 
-                    db_location = LocationModel(
-                        ward_id=db_ward.id,
-                    )
-                    db.add(db_location)
-                    db.commit()
-                    db.refresh(db_location)
+        except Exception as error:
+            raise HTTPException(status_code=500, detail=f"Network error: {str(error)}")
