5889cbcdd5fd7fb1dce4c2673d12b534ff7d84d1
ctpanh <ctpanh03@gmail.com>
2023-12-20 17:03:23 +0700

    feat: table transaction

--- client/src/app/staff-gathering/receive/page.tsx
+++ client/src/app/staff-gathering/receive/page.tsx
@@ -1,3 +1,55 @@
+import Table from "@/components/table";
+
 export default function StaffGathering() {
-  return <div>StaffGathering receive</div>;
+  const data = [
+    {
+      id: 1,
+      code: "MP264989219012",
+      transaction_type: "Deliver",
+      status: "processing",
+    },
+    {
+      id: 2,
+      code: "MP264989219012",
+      transaction_type: "Deliver",
+      status: "processing",
+    },
+    {
+      id: 3,
+      code: "MP264989219012",
+      transaction_type: "Deliver",
+      status: "processing",
+    },
+    {
+      id: 4,
+      code: "MP264989219012",
+      transaction_type: "Deliver",
+      status: "processing",
+    },
+    {
+      id: 5,
+      code: "MP264989219012",
+      transaction_type: "Deliver",
+      status: "processing",
+    },
+    {
+      id: 6,
+      code: "MP264989219012",
+      transaction_type: "Deliver",
+      status: "processing",
+    },
+    {
+      id: 7,
+      code: "MP264989219012",
+      transaction_type: "Deliver",
+      status: "processing",
+    },
+  ];
+  const headers = ["STT", "Mã đơn hàng", "Loại đơn hàng", "Tình trạng", "Chi tiết"];
+  return (
+    <>
+      <div>StaffGathering receive</div>
+      <Table headers={headers} data={data} rowsPerPage={5} />
+    </>
+  );
 }
