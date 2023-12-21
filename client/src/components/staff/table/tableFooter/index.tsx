92cf27732e2f1822b52185602f416529c0289f24
ctpanh <ctpanh03@gmail.com>
2023-12-21 13:18:26 +0700

    fix: cursor footer table

--- client/src/components/staff/table/tableFooter/index.tsx
+++ client/src/components/staff/table/tableFooter/index.tsx
@@ -11,10 +11,10 @@ interface Props {
 const TableFooter: React.FC<Props> = ({ range, setPage, page, slice }) => {
   const styles = {
     footer: "flex items-center justify-end gap-2",
-    button:
-      "w-8 me-1 h-8 flex items-center justify-center cursor-pointer text-stone-600",
+    button: "w-8 me-1 h-8 flex items-center justify-center text-stone-600",
     active: "border rounded-md border-stone-600",
-    hover: "hover:bg-neutral-400 hover:text-white hover:boder hover:rounded-md",
+    hover:
+      "hover:bg-neutral-400 hover:text-white hover:boder hover:rounded-md hover:cursor-pointer",
   };
   useEffect(() => {
     if (slice.length < 1 && page !== 1) {
