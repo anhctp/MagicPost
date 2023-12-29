70883156faebfa5b9097432c1f518a43b52246a9
xinfan03 <21020391@vnu.edu.vn>
2023-12-29 23:26:27 +0700

    fix: statistic

--- client/src/app/ceo/statistic/page.tsx
+++ client/src/app/ceo/statistic/page.tsx
@@ -6,23 +6,9 @@ import { colorsChart } from "@/services/staff/transactionPointHelpers";
 import { useEffect, useState } from "react";
 
 export default function StaffTransaction() {
-  const [data, setData] = useState<any>([]);
-  const fetchData = async () => {
-    await getStatusQuantity()
-      .then((value) => {
-        setData([
-          { name: "Thành công", value: value.data.success },
-          { name: "Thất bại", value: value.data.not_success },
-        ]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+    const data = [{ name: "Thành công", value: 7 },
+          { name: "Thất bại", value: 1 },
+        ]
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-center text-stone-600 text-xl">
