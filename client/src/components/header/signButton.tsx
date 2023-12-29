99926bad0c0cef631cb30ae94b6ada008a20efe3
ctpanh <ctpanh03@gmail.com>
2023-12-29 20:37:35 +0700

    fix: navbar

--- client/src/components/header/signButton.tsx
+++ client/src/components/header/signButton.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import useAuthStore from "@/stores/authStore";
 import { useRouter } from "next/navigation";
+import { MapPinIcon, UserCircleIcon } from "@heroicons/react/24/outline";
+import { useUserStore } from "@/stores/userStore";
+import { Role } from "@/services/user/userHelper";
 
 export default function SignButton() {
   const { authorized } = useAuthStore();
+  const { userRole } = useUserStore();
   const logout = useAuthStore((state) => state.logout);
   const router = useRouter();
   const handleLogout = () => {
@@ -11,8 +15,24 @@ export default function SignButton() {
     router.push("/");
   };
   return authorized ? (
-    <div className="cursor-pointer" onClick={handleLogout}>
-      Signed
+    <div className="flex gap-4 justify-end items-center text-xl text-neutral-400">
+      <div className="cursor-pointer text-gray" onClick={handleLogout}>
+        <UserCircleIcon width={50} height={50} />
+      </div>
+      {userRole === Role.LEADERGATHERING || userRole === Role.STAFFGATHERING ? (
+        <div className="flex justify-center items-center">
+          <MapPinIcon width={30} height={30} />
+          Điểm tập kết
+        </div>
+      ) : userRole === Role.LEADERTRANSACTION ||
+        userRole === Role.STAFFTRANSACTION ? (
+        <div className="flex justify-center items-center">
+          <MapPinIcon width={30} height={30} />
+          Điểm giao dịch{" "}
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   ) : (
     <div className="w-fit justify-end inline-flex text-center text-sm text-stone-600 font-semibold">
